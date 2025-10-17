bb9a3864d138157588b6efb0ddc568f36271608e
roaziy <erkhemtur389@gmail.com>
2025-10-17 10:44:26 +0800

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <h1 className="text-3xl font-bold">Hello, Next.js!</h1>
     </div>
   );
 }
